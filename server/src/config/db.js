39e24c5e7e3bdaf9902ecf58407bfa9890edb351
Saad Waheed <saadwaheed67@gmail.com>
2026-05-17 01:41:29 +0500

    fix mongoose deprecated options

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
-    throw error;
+    console.log("DB Connection Failed");
+    process.exit(1);
   }
 };
 
