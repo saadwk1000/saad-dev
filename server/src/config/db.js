d9edc65d2eda33fba2a6b882e3e3310fe657da27
Saad Waheed <saadwaheed67@gmail.com>
2026-05-17 00:44:48 +0500

    debug mongo uri

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
@@ -10,5 +12,3 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-module.exports = connectDB;
