c42479557776c65902ce66b0e8defee832a63284
Saad Waheed <saadwaheed67@gmail.com>
2026-05-19 16:31:22 +0500

    Minor Fixes

--- client/src/pages/projects/page.tsx
+++ client/src/pages/projects/page.tsx
@@ -14,13 +14,6 @@ const Projects = () => (
           window.open("https://github.com/saadwk1000/system-admin-mockHCM.git");
         }}
       />
-      <ProjectCard
-        title="MyRoster - Director"
-        description="Built a sports roster management prototype using Next.js and Cursor. The platform supports multiple registered teams with dedicated Parent, Player, and Coach workflows for managing player information and team interactions."
-        onClick={() => {
-          window.open("https://github.com/saadwk1000/MyRoster-Director.git");
-        }}
-      />
     </div>
   </section>
 );
